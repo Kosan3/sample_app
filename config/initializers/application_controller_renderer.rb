2abdd4540098cad75fe505691fd4b7745c4cf288
Kosan3 <ichiro.neko.0421@gmail.com>
2020-05-03 10:43:05 +0900

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '4841f7fdc25a7f1c31f1335bfc8d52096e2151edf7cfd62514de9366966683b4acfefb81a57cfcade3fa17b41b0e28b40bfcfd587d7caae95061399589dd3392'
